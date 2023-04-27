8277883e281fd1264aab5e60d15ab1c0c60a876d
Olivier Delannoy <olivier.delannoy@gmail.com>
2023-04-27 20:04:31 +0100

    Update index.ts

--- src/config/translations/en/index.ts
+++ src/config/translations/en/index.ts
@@ -240,7 +240,7 @@ englishLanguage = {
         },
         directory: {
             title: "Our Directory",
-            body: "<TBD>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            body: "",
             navToDirectory: "Find your counsellor or therapist",
         },
         focusArea: {
