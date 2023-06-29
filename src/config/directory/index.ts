140d21bc02801e9a9c8acc53b6512eb1fafe7a18
Georgina Haynes <110546217+Haynesgir@users.noreply.github.com>
2023-06-29 17:27:09 +0100

    Update index.ts

--- src/config/directory/index.ts
+++ src/config/directory/index.ts
@@ -3,11 +3,21 @@ import ProfessionalModel from "../../libs/models/Professional";
 import { SusanneFuller } from './susanne-fuller'
 import { IsabelleDelannoy } from './isabelle-delannoy'
 import { ValeriaVilla } from './valeria-villa'
+import { HenriettaWhitfield } from './henrietta-whitfield'
+import { LucyWalthoe } from './lucy-walthoe'
+import { LynneBall } from './lynne-ball'
+import { MihaelaHartescu} from './mihaela-hartescu'
+import { SarahCrowley } from './sarah-crowley'
 
 const professionals = new Map<string, ProfessionalModel>([
         [SusanneFuller.id,  SusanneFuller],
         [IsabelleDelannoy.id, IsabelleDelannoy],
         [ValeriaVilla.id, ValeriaVilla],
+        [HenriettaWhitfield.id, HenriettaWhitfield],
+        [LucyWalthoe.id, LucyWalthoe],
+        [LynnaBall.id, LynneBall],
+        [MihaelaHartescu.id, MihaelaHartescu],
+        [SarahCrowley.id, SarahCrowley],
     ])
 
 export default professionals
